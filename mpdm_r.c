381e21fe7487db4ed72ae821753766328f3d0d89
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2004-05-14 17:53:21 +0000

    New mpdm_regex() function.
    Compiled regexes are stored in a cache.
    
    
    git-svn-id: file:///home/angel/tmp/svn-triptico/mpdm/trunk@1114 c87de0a0-a11c-0410-a1e5-866214bc28b2

--- mpdm_r.c
+++ mpdm_r.c
@@ -44,54 +44,154 @@
 #endif
 
 /*******************
+	Data
+********************/
+
+/* the regex cache */
+mpdm_v _mpdm_regex_cache=NULL;
+mpdm_v _mpdm_iregex_cache=NULL;
+
+/*******************
 	Code
 ********************/
 
+mpdm_v _mpdm_regcomp(mpdm_v r, char * flags)
+{
+	mpdm_v cr;
+	mpdm_v c;
+	int f;
+
+	/* if cache does not exist, create it */
+	if(_mpdm_regex_cache == NULL)
+	{
+		mpdm_ref(_mpdm_regex_cache=MPDM_H(0));
+		mpdm_ref(_mpdm_iregex_cache=MPDM_H(0));
+	}
+
+	/* flags */
+	if(flags != NULL && strchr(flags, 'i') != NULL)
+	{
+		c=_mpdm_iregex_cache;
+		f=REG_ICASE;
+	}
+	else
+	{
+		c=_mpdm_regex_cache;
+		f=0;
+	}
+
+	/* search the regex in the cache */
+	if((cr=mpdm_hget(c, r)) == NULL)
+	{
+		/* not found; regex must be compiled */
+		cr=mpdm_new(MPDM_COPY, NULL, sizeof(regex_t));
+
+		if(regcomp((regex_t *) cr->data,
+			(char *) r->data, REG_EXTENDED | f))
+			return(NULL);
+
+		/* stores */
+		mpdm_hset(c, r, cr);
+	}
+
+	return(cr);
+}
+
+
+/**
+ * mpdm_regex - Matches a regular expression
+ * @r: the regular expression
+ * @v: the value to be matched
+ * @offset: offset from the start of v->data
+ * @flags: flags
+ *
+ * Matches a regular expression against a value. @flags is a char *
+ * that can contain an 'i' for case-insensitive matches, or NULL.
+ *
+ * If the regex is matched, returns a two value array containing
+ * the start and stop offsets of the matched string, or NULL otherwise.
+ */
+mpdm_v mpdm_regex(mpdm_v r, mpdm_v v, int offset, char * flags)
+{
+	mpdm_v cr;
+	mpdm_v w=NULL;
+	regmatch_t rm;
+
+	/* compile the regex */
+	if((cr=_mpdm_regcomp(r, flags)) != NULL)
+	{
+		/* match? */
+		if(regexec((regex_t *) cr->data,
+			(char *) v->data + offset, 1,
+			&rm, offset > 0 ? REG_NOTBOL : 0) == 0)
+		{
+			/* found! */
+			w=MPDM_A(2);
+
+			mpdm_aset(w, MPDM_I(offset + rm.rm_so), 0);
+			mpdm_aset(w, MPDM_I(offset + rm.rm_eo), 1);
+		}
+	}
+
+	return(w);
+}
+
+
+/**
+ * mpdm_sregex - Matches and substitutes a regular expression
+ * @r: the regular expression
+ * @v: the value to be matched
+ * @s: the string that will substitute the matched string
+ * @offset: offset from the start of v->data
+ * @flags: flags
+ *
+ * Matches a regular expression against a value, and substitutes the
+ * found substring with @s. @flags is a char * that can contain an 'i'
+ * for case-insensitive matches, a 'g' for a global substitution
+ * (substitute all ocurrences of the regex, not only the first one), or NULL.
+ *
+ * Returns the modified string, or the original one if no substitutions
+ * were done.
+ */
 mpdm_v mpdm_sregex(mpdm_v r, mpdm_v v, mpdm_v s, int offset, char * flags)
 {
+	mpdm_v cr;
 	char * ptr;
-	regex_t re;
 	regmatch_t rm;
-	int f=0;
-	int i,g;
+	int f, i;
+	int g=0;
 
 	/* flags */
 	if(flags != NULL)
-	{
 		g=strchr(flags, 'g') != NULL ? 1 : 0;
-		i=strchr(flags, 'i') != NULL ? REG_ICASE : 0;
-	}
-	else
-		i=g=0;
 
-	/* compile the regex or fail */
-	if(regcomp(&re, (char *) r->data, REG_EXTENDED | i))
-		return(NULL);
-
-	do
+	/* compile the regex */
+	if((cr=_mpdm_regcomp(r, flags)) != NULL)
 	{
-		ptr=(char *) v->data + offset;
-
-		/* try match */
-		f=!regexec(&re, ptr, 1, &rm, offset > 0 ? REG_NOTBOL : 0);
-
-		if(f)
+		do
 		{
-			/* size of the found string */
-			i=rm.rm_eo - rm.rm_so;
+			ptr=(char *) v->data + offset;
 
-			/* do the substitution */
-			v=mpdm_splice(v, s, offset + rm.rm_so, i);
-			v=mpdm_aget(v, 0);
+			/* try match */
+			f=!regexec((regex_t *) cr->data, ptr,
+				1, &rm, offset > 0 ? REG_NOTBOL : 0);
 
-			/* move on */
-			offset+=rm.rm_so;
-			if(s != NULL) offset+=s->size;
-		}
+			if(f)
+			{
+				/* size of the found string */
+				i=rm.rm_eo - rm.rm_so;
 
-	} while(f && g);
+				/* do the substitution */
+				v=mpdm_splice(v, s, offset + rm.rm_so, i);
+				v=mpdm_aget(v, 0);
 
-	regfree(&re);
+				/* move on */
+				offset+=rm.rm_so;
+				if(s != NULL) offset+=s->size;
+			}
+
+		} while(f && g);
+	}
 
 	return(v);
 }
