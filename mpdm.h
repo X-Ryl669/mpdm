fdaae54a59f06776d8a599c868d5e20e55598084
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2004-12-20 16:02:32 +0000

    mpdm_write() has the end of line separator as argument (Closes: #1044).

--- mpdm.h
+++ mpdm.h
@@ -149,7 +149,7 @@ void _mpdm_write_wcs(FILE * f, wchar_t * str);
 mpdm_v mpdm_open(mpdm_v filename, mpdm_v mode);
 mpdm_v mpdm_close(mpdm_v fd);
 mpdm_v mpdm_read(mpdm_v fd);
-int mpdm_write(mpdm_v fd, mpdm_v v);
+int mpdm_write(mpdm_v fd, mpdm_v v, mpdm_v eol);
 int mpdm_unlink(mpdm_v filename);
 mpdm_v mpdm_glob(mpdm_v spec);
 
