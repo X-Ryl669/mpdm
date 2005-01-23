88e2a1598134c4c2a31207a7d14e0772281b72b5
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2005-01-23 17:40:01 +0000

    Copyright years updated.

--- mpdm.h
+++ mpdm.h
@@ -1,7 +1,7 @@
 /*
 
     mpdm - Minimum Profit Data Manager
-    Copyright (C) 2003/2004 Angel Ortega <angel@triptico.com>
+    Copyright (C) 2003/2005 Angel Ortega <angel@triptico.com>
 
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License
