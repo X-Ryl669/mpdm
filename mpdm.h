e4cc470d3856812ebc7b1c68843800da9840fbbb
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2006-10-12 07:55:14 +0000

    New functions mpdm_fseek() and mpdm_ftell() (Closes: #1092, #1093).

--- mpdm.h
+++ mpdm.h
@@ -150,6 +150,8 @@ mpdm_t mpdm_read(mpdm_t fd);
 int mpdm_write(mpdm_t fd, mpdm_t v);
 mpdm_t mpdm_getchar(mpdm_t fd);
 mpdm_t mpdm_putchar(mpdm_t fd, mpdm_t c);
+int mpdm_fseek(mpdm_t fd, long offset, int whence);
+long mpdm_ftell(mpdm_t fd);
 int mpdm_encoding(mpdm_t charset);
 int mpdm_unlink(mpdm_t filename);
 mpdm_t mpdm_stat(mpdm_t filename);
