f8e8ff07f7c9aef6e5969fbdb098032eb95043ca
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2006-11-11 10:19:01 +0000

    New function mpdm_dumper(), the same as mpdm_dump() but returning the dump as an mpdm string value.
    git-svn-id: file:///home/angel/tmp/svn-triptico/mpdm/trunk@5800 c87de0a0-a11c-0410-a1e5-866214bc28b2

--- mpdm.h
+++ mpdm.h
@@ -134,6 +134,7 @@ mpdm_t mpdm_hset_s(mpdm_t h, wchar_t * k, mpdm_t v);
 mpdm_t mpdm_hdel(mpdm_t h, mpdm_t k);
 mpdm_t mpdm_keys(mpdm_t h);
 
+mpdm_t mpdm_dumper(mpdm_t v);
 void mpdm_dump(mpdm_t v);
 void mpdm_dump_unref(void);
 
