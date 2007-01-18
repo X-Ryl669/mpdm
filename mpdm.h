512e5171a8032080eeee2baff8776b1a4a86479e
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2007-01-18 07:24:01 +0000

    Updated copyright notice.
    git-svn-id: file:///home/angel/tmp/svn-triptico/mpdm/trunk@5877 c87de0a0-a11c-0410-a1e5-866214bc28b2

--- mpdm.h
+++ mpdm.h
@@ -1,7 +1,7 @@
 /*
 
     MPDM - Minimum Profit Data Manager
-    Copyright (C) 2003/2006 Angel Ortega <angel@triptico.com>
+    Copyright (C) 2003/2007 Angel Ortega <angel@triptico.com>
 
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License
