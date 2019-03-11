d4a1142c9fadce663413af44b7ae78d99443eef7
Angel Ortega <angel+remote@triptico.com>
2019-03-11 14:37:02 +0100

    Moved old hash interface.

--- mpdm_h.c
+++ mpdm_h.c
@@ -21,66 +21,6 @@
 
 /** code **/
 
-/* default hash buckets (must be prime) */
-static int mpdm_hash_buckets = 31;
-
-/* prototype for the one-time wrapper hash function */
-static int switch_hash_func(const wchar_t *, int);
-
-/* pointer to the hashing function */
-static int (*mpdm_hash_func) (const wchar_t *, int) = switch_hash_func;
-
-static int standard_hash_func(const wchar_t *string, int mod)
-/* computes a hashing function on string */
-{
-    int c;
-
-    for (c = 0; *string != L'\0'; string++)
-        c ^= (int) *string;
-
-    return c % mod;
-}
-
-
-static int null_hash_func(const wchar_t *string, int mod)
-{
-    return *string % mod;
-}
-
-static int switch_hash_func(const wchar_t *string, int mod)
-/* one-time wrapper for hash method autodetection */
-{
-    /* commute the real hashing function on
-       having the MPDM_NULL_HASH environment variable set */
-    if (getenv("MPDM_NULL_HASH") != NULL)
-        mpdm_hash_func = null_hash_func;
-    else
-        mpdm_hash_func = standard_hash_func;
-
-    /* and fall back to it */
-    return mpdm_hash_func(string, mod);
-}
-
-#define HASH_BUCKET_S(h, k) mpdm_hash_func(k, mpdm_size(h))
-#define HASH_BUCKET(h, k)   mpdm_hash_func(mpdm_string(k), mpdm_size(h))
-
-/* interface */
-
-
-mpdm_t mpdm_new_h(int size)
-/* creates a new hash value */
-{
-    mpdm_t v;
-
-    /* creates and expands */
-    v = mpdm_new(MPDM_TYPE_OBJECT, NULL, 0);
-
-    mpdm_expand(v, 0, size);
-
-    return v;
-}
-
-
 /**
  * mpdm_hsize - Returns the number of pairs of a hash.
  * @h: the hash
@@ -90,15 +30,7 @@ mpdm_t mpdm_new_h(int size)
  */
 int mpdm_hsize(const mpdm_t h)
 {
-    int n;
-    int ret = 0;
-
-    for (n = 0; n < mpdm_size(h); n++) {
-        mpdm_t b = mpdm_aget(h, n);
-        ret += mpdm_size(b);
-    }
-
-    return ret / 2;
+    return mpdm_count_o(h);
 }
 
 
@@ -113,20 +45,7 @@ int mpdm_hsize(const mpdm_t h)
  */
 mpdm_t mpdm_hget_s(const mpdm_t h, const wchar_t *k)
 {
-    mpdm_t b;
-    mpdm_t v = NULL;
-    int n = 0;
-
-    if (mpdm_size(h)) {
-        /* if hash is not empty... */
-        if ((b = mpdm_aget(h, HASH_BUCKET_S(h, k))) != NULL)
-            n = mpdm_bseek_wcs(b, k, 2, NULL);
-
-        if (n >= 0)
-            v = mpdm_aget(b, n + 1);
-    }
-
-    return v;
+    return mpdm_get_wcs(h, k);
 }
 
 
@@ -141,13 +60,7 @@ mpdm_t mpdm_hget_s(const mpdm_t h, const wchar_t *k)
  */
 mpdm_t mpdm_hget(const mpdm_t h, const mpdm_t k)
 {
-    mpdm_t r;
-
-    mpdm_ref(k);
-    r = mpdm_hget_s(h, mpdm_string(k));
-    mpdm_unref(k);
-
-    return r;
+    return mpdm_get_o(h, k);
 }
 
 
@@ -164,52 +77,7 @@ mpdm_t mpdm_hget(const mpdm_t h, const mpdm_t k)
  */
 mpdm_t mpdm_hset(mpdm_t h, mpdm_t k, mpdm_t v)
 {
-    mpdm_t b, r;
-    int n;
-
-    mpdm_ref(k);
-    mpdm_ref(v);
-
-    /* if hash is empty, create an optimal number of buckets */
-    if (mpdm_size(h) == 0)
-        mpdm_expand(h, 0, mpdm_hash_buckets);
-
-    n = HASH_BUCKET(h, k);
-
-    if ((b = mpdm_aget(h, n)) != NULL) {
-        int pos;
-
-        /* bucket exists; try to find the key there */
-        n = mpdm_bseek(b, k, 2, &pos);
-
-        if (n < 0) {
-            /* the pair does not exist: create it */
-            n = pos;
-            mpdm_expand(b, n, 2);
-
-            mpdm_aset(b, k, n);
-        }
-    }
-    else {
-        /* the bucket does not exist; create it */
-        b = MPDM_A(2);
-
-        /* put the bucket into the hash */
-        mpdm_aset(h, b, n);
-
-        /* offset 0 */
-        n = 0;
-
-        /* insert the key */
-        mpdm_aset(b, k, n);
-    }
-
-    r = mpdm_aset(b, v, n + 1);
-
-    mpdm_unref(v);
-    mpdm_unref(k);
-
-    return r;
+    return mpdm_set_o(h, v, k);
 }
 
 
@@ -226,7 +94,7 @@ mpdm_t mpdm_hset(mpdm_t h, mpdm_t k, mpdm_t v)
  */
 mpdm_t mpdm_hset_s(mpdm_t h, const wchar_t *k, mpdm_t v)
 {
-    return mpdm_hset(h, MPDM_S(k), v);
+    return mpdm_set_wcs(h, v, k);
 }
 
 
@@ -241,24 +109,7 @@ mpdm_t mpdm_hset_s(mpdm_t h, const wchar_t *k, mpdm_t v)
  */
 mpdm_t mpdm_hdel(mpdm_t h, const mpdm_t k)
 {
-    mpdm_t b;
-    int n;
-
-    mpdm_ref(k);
-
-    if (mpdm_size(h)) {
-        if ((b = mpdm_aget(h, HASH_BUCKET(h, k))) != NULL) {
-            /* bucket exists */
-            if ((n = mpdm_bseek(b, k, 2, NULL)) >= 0) {
-                /* collapse the bucket */
-                mpdm_collapse(b, n, 2);
-            }
-        }
-    }
-
-    mpdm_unref(k);
-
-    return NULL;
+    return mpdm_del_o(h, k);
 }
 
 
@@ -272,19 +123,5 @@ mpdm_t mpdm_hdel(mpdm_t h, const mpdm_t k)
  */
 mpdm_t mpdm_keys(const mpdm_t h)
 {
-    int c;
-    mpdm_t a, i;
-
-    mpdm_ref(h);
-
-    /* create an array with the same number of elements */
-    a = MPDM_A(0);
-
-    c = 0;
-    while (mpdm_iterator(h, &c, NULL, &i))
-        mpdm_push(a, i);
-
-    mpdm_unref(h);
-
-    return a;
+    return mpdm_indexes(h);
 }
--- mpdm_o.c
+++ mpdm_o.c
@@ -361,3 +361,111 @@ int mpdm_iterator_o(mpdm_t set, int *context, mpdm_t *v, mpdm_t *i)
 
     return ret;
 }
+
+
+/* old hash compatibility layer */
+
+/**
+ * mpdm_hsize - Returns the number of pairs of a hash.
+ * @h: the hash
+ *
+ * Returns the number of key-value pairs of a hash.
+ * [Hashes]
+ */
+int mpdm_hsize(const mpdm_t h)
+{
+    return mpdm_count_o(h);
+}
+
+
+/**
+ * mpdm_hget_s - Gets the value from a hash (string version).
+ * @h: the hash
+ * @k: the key
+ *
+ * Gets the value from the hash @h having @k as key, or
+ * NULL if the key does not exist.
+ * [Hashes]
+ */
+mpdm_t mpdm_hget_s(const mpdm_t h, const wchar_t *k)
+{
+    return mpdm_get_wcs(h, k);
+}
+
+
+/**
+ * mpdm_hget - Gets a value from a hash.
+ * @h: the hash
+ * @k: the key
+ *
+ * Gets the value from the hash @h having @k as key, or
+ * NULL if the key does not exist.
+ * [Hashes]
+ */
+mpdm_t mpdm_hget(const mpdm_t h, const mpdm_t k)
+{
+    return mpdm_get_o(h, k);
+}
+
+
+/**
+ * mpdm_hset - Sets a value in a hash.
+ * @h: the hash
+ * @k: the key
+ * @v: the value
+ *
+ * Sets the value @v to the key @k in the hash @h. Returns
+ * the new value (versions prior to 1.0.10 returned the old
+ * value).
+ * [Hashes]
+ */
+mpdm_t mpdm_hset(mpdm_t h, mpdm_t k, mpdm_t v)
+{
+    return mpdm_set_o(h, v, k);
+}
+
+
+/**
+ * mpdm_hset_s - Sets a value in a hash (string version).
+ * @h: the hash
+ * @k: the key
+ * @v: the value
+ *
+ * Sets the value @v to the key @k in the hash @h. Returns
+ * the new value (versions prior to 1.0.10 returned the old
+ * value).
+ * [Hashes]
+ */
+mpdm_t mpdm_hset_s(mpdm_t h, const wchar_t *k, mpdm_t v)
+{
+    return mpdm_set_wcs(h, v, k);
+}
+
+
+/**
+ * mpdm_hdel - Deletes a key from a hash.
+ * @h: the hash
+ * @k: the key
+ *
+ * Deletes the key @k from the hash @h. Returns NULL
+ * (versions prior to 1.0.10 returned the deleted value).
+ * [Hashes]
+ */
+mpdm_t mpdm_hdel(mpdm_t h, const mpdm_t k)
+{
+    return mpdm_del_o(h, k);
+}
+
+
+/**
+ * mpdm_keys - Returns the keys of a hash.
+ * @h: the hash
+ *
+ * Returns an array containing all the keys of the @h hash.
+ * [Hashes]
+ * [Arrays]
+ */
+mpdm_t mpdm_keys(const mpdm_t h)
+{
+    return mpdm_indexes(h);
+}
