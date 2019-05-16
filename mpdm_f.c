166365689eb76e7eb8b51f2a2e717d904c0693e4
Angel Ortega <angel+remote@triptico.com>
2019-05-16 11:21:45 +0200

    Do not process utf-8 prefix 0xf0.
    wchar_t in MS Windows is 16bit, so it won't work anyway.

--- mpdm_f.c
+++ mpdm_f.c
@@ -386,6 +386,7 @@ static wchar_t *read_utf8(struct mpdm_file *f, int *s, int *eol)
             BYTE_OR_BREAK(c, f);
             wc |= (c & 0x3f);
         }
+#ifndef CONFOPT_WIN32
         else
         if ((c & 0xf8) == 0xf0) {
             wc = (c & 0x07) << 18;
@@ -396,6 +397,7 @@ static wchar_t *read_utf8(struct mpdm_file *f, int *s, int *eol)
             BYTE_OR_BREAK(c, f);
             wc |= (c & 0x3f);
         }
+#endif
 
         if (store_in_line(&ptr, s, eol, wc))
             break;
